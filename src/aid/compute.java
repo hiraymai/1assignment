9c7f7e57e81ffa261a966a188920e379f4a87bd6
yerkenaz <erkenazsultanova@gmail.com>
2023-12-24 01:13:34 +0600

    added classes "point" and "shape"

---
+++ src/aid/compute.java
@@ -0,0 +1,35 @@
+package aid;
+
+public class compute {
+    private compute() {
+
+    }
+    public static double getMax(double[] arr) {
+        if (arr.length == 0)
+            return 0;
+
+        double max = arr[0];
+        for (int i = 1; i<arr.length; i++) {
+            if(max<arr[i])
+                max = arr[i];
+        }
+
+        return max;
+    }
+
+    public static double getSum(double[] arr) {
+        double sum = 0;
+        for (double v : arr) {
+            sum += v;
+        }
+
+        return sum;
+    }
+    public static double getAvg(double[] arr) {
+        if(arr.length == 0) {
+            return 0;
+        }
+
+        return getSum(arr)/ arr.length;
+    }
+}
